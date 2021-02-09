276ace82af4bd0e1ededc7cff39aa8b80712f496
Samuel DeSantis <samuel.i.desantis@gmail.com>
2021-02-09 02:48:24 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,20 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :hp, :db
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @hp = hp
+    @db = db
+  end
+
+  def save
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", self.name, self.type)
+  end
+
+  def find
+    pokemon_info = db.execute("SELECT * FROM pokemon WHERE id = ?", self.name, self.type).flatten
+    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: pokemon_info[3])
+  end
 end
